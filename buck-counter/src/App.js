565631933914736d98b43440879724babfc7288a
hchachra2 <113888718+hchachra2@users.noreply.github.com>
2023-07-08 17:25:04 +0530

    header, nav bar and pages
    -router
    -i18next
    -scss

--- buck-counter/src/App.js
+++ buck-counter/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import HeaderBar from './components/common/headerBar';
+import { initReactI18next } from 'react-i18next';
+import i18n from 'i18next';
+import enTranslations from './locales/en.json';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import Accounts from './pages/accounts';
+import Transactions from './pages/transactions';
+
+// Initialize i18next
+i18n.use(initReactI18next).init({
+  resources: {
+    en: { translation: enTranslations },
+  },
+  lng: 'en', // Set the default language
+  fallbackLng: 'en', // Fallback language if a translation is missing
+  interpolation: {
+    escapeValue: false, // React already escapes string content
+  },
+});
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HeaderBar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/accounts" element={<Accounts />} />
+        <Route exact path="/transactions" element={<Transactions />} />
+      </Routes>
     </div>
   );
 }
