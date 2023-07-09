05a8bdd2e436dbd2ff093fb7608ff170b34f002c
hchachra2 <113888718+hchachra2@users.noreply.github.com>
2023-07-09 11:44:42 +0530

    delete components

--- buck-counter/src/components/pages/accounts.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Accounts() {
-  return <div>Accounts Page</div>;
-}
-
-export default Accounts;
--- buck-counter/src/components/pages/transactions.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Transactions() {
-  return <div>Transactions Page</div>;
-}
-
-export default Transactions;
